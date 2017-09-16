9c8992f3d4f12f3e39321f79142148b4d9085ea7
Karim M. Ibrahim <karim.m.ibraheem@gmail.com>
2017-09-16 16:08:44 +0800

    Network with variable number of layers complete

--- NeuralNetworkTrainer.py
+++ NeuralNetworkTrainer.py
@@ -50,12 +50,10 @@ def relu_diff(z):
 """
 Initializing the network
 """
-def init_net(inputs,hidden,outputs):
+def init_net(*args):
     network = list()
-    input_to_hidden = np.random.rand(hidden,inputs+1)
-    hidden_to_output = np.random.rand(outputs,hidden+1)
-    network.append(input_to_hidden)
-    network.append(hidden_to_output)
+    for layer_number in range(1,len(args)):
+        network.append(np.random.rand(args[layer_number],args[layer_number-1]+1)) # adding 1 for the bias term
     return network
     
 """
@@ -63,27 +61,34 @@ forward propagation
 """
 def forward_prop(net,inputs):
     inputs = np.append(inputs,1)
-    H_hidden = relu(np.dot(net[0],inputs))
-    H_hidden = np.append(H_hidden,1)
-    #Outputs = sigmoid(np.dot(net[1],H_hidden))
-    Outputs = softmax(np.dot(net[1],H_hidden))
-    return [H_hidden, Outputs]
+    Nodes_state = []
+    for counter in range(0,len(net)-1):
+        if (counter == 0):
+            H_hidden = relu(np.dot(net[counter],inputs))
+        else: 
+            H_hidden = relu(np.dot(net[counter],H_hidden))
+        H_hidden = np.append(H_hidden,1)
+        Nodes_state.append(H_hidden)
+    Outputs = softmax(np.dot(net[-1],H_hidden))
+    Nodes_state.append(Outputs)
+    return Nodes_state
 
 """
 back propagation
 """
 def back_prop(network,expected,inputs,l_rate):
-    [hidden_output, outputs] = forward_prop(network,inputs)
+    Nodes_state = forward_prop(network,inputs)
     inputs = np.append(inputs,1)
-    dE_dO = expected - outputs
+    dE_dO = expected - Nodes_state[-1]
     #dE_dZo = dE_dO * sigmoid_diff(outputs) # For Sigmoid
-    dE_dZo = dE_dO                          # For softmax
-    dE_dWh = np.dot(np.reshape(dE_dZo,[len(dE_dZo),1]),np.reshape(hidden_output,[1,len(hidden_output)])) # I want [dE_dZo] as a vertical vector dot produt by hidden outputs as horizontal -> this gives weight update for each weight
-    dE_dZh = np.dot(dE_dZo,network[1][:,:-1]) * relu_diff(hidden_output[:-1])
-    dE_dWo = np.dot(np.reshape(dE_dZh,[len(dE_dZh),1]),np.reshape(inputs,[1,len(inputs)])) # same as dE_dWh [figure out]
-    #updating weights
-    network[1] += l_rate * dE_dWh
-    network[0] += l_rate * dE_dWo
+    dE_dZ = dE_dO                          # For softmax
+    for counter in range(len(network)-1,0,-1):
+        hidden_output = Nodes_state[counter-1]
+        dE_dW = np.dot(np.reshape(dE_dZ,[len(dE_dZ),1]),np.reshape(hidden_output,[1,len(hidden_output)])) # I want [dE_dZo] as a vertical vector dot produt by hidden outputs as horizontal -> this gives weight update for each weight
+        dE_dZ = np.dot(dE_dZ,network[counter][:,:-1]) * relu_diff(hidden_output[:-1]) # hidden_output[:-1] to exclude the bias
+        network[counter] += l_rate * dE_dW
+    dE_dW = np.dot(np.reshape(dE_dZ,[len(dE_dZ),1]),np.reshape(inputs,[1,len(inputs)]))
+    network[0] += l_rate * dE_dW
 
 """
 Training the network
@@ -140,6 +145,7 @@ def testingNetwork(dataSet,network):
     actual = [row[-1] for row in dataSet]
     return accuracy_metric(actual,predictions)
 
+train(net,TrainData[0:100],1000,4,0.01)
 
 """
 x_training = genfromtxt('../data/Question2_123/x_train.csv',delimiter = ",")
